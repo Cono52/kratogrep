ddb1664098cd2a816ed18d0317adcb6f2d6ee12e
Conor O'Flanagan <oflanac52@gmail.com>
2019-07-28 23:02:12 +0100

    wrap search term

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
+    println!("Searching for '{}'", config.query);
     println!("In file {}", config.filename);
 
     if let Err(e) = kratogrep::run(config) {
